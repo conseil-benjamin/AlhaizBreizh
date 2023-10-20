bff86438499b6b244647996900f401f98a846e0c
MrMiamm <cherwill@laposte.net>
2023-10-20 15:12:02 +0200

    Header on top

--- index.php
+++ index.php
@@ -48,37 +48,7 @@
                 ?>
             </div> 
         </div>   
-        <footer id="footer">
-            <img src="/public/icons/wave.svg">
-            <div>
-                <div>
-                    <div>
-                        <h2>Assistance</h2>
-                        <ul>
-                            <li><a href="">Centre d'aide</a></li>
-                            <li><a href="">Options d'annulation</a></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>Accueil des voyageurs</h2>
-                        <ul>
-                            <li><a href="">Nous rejoindre</a></li>
-                            <li><a href="">Mettez votre logement sur ALHaIZ Breizh</a></li>
-                            <li><a href="">Confiance et sécurité</a></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>ALHaIZ Breizh</h2>
-                        <ul>
-                            <li><a href="">Qui sommes-nous</a></li>
-                            <li><a href="">Conditions générales d'utilisation</a></li>
-                            <li><a href="">Mentions légales</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <p>© 2023 ALHaIZ Breizh - Tous droits réservés</p>
-            </div>
-        </footer>
+        <?php include './src/php/footer.php'; ?>
         <script src="/src/js/compte-click.js"></script>
         <script>
             window.addEventListener("scroll", () => {
--- src/js/compte-click.js
+++ src/js/compte-click.js
@@ -5,9 +5,11 @@ let isCompteOptionsVisible = false;
 compteDiv.addEventListener('click', () => {
     if (!isCompteOptionsVisible) {
         compteOptionsDiv.style.display = 'block';
+        compteDiv.classList.add('shown');
         isCompteOptionsVisible = true;
     } else {
         compteOptionsDiv.style.display = 'none';
+        compteDiv.classList.remove('shown');
         isCompteOptionsVisible = false;
     }
 });
