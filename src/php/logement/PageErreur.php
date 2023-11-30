50a9dca52d26cc0581eb8a75e4acf2f5378df8a0
Nathan Bracquart <122649974+nbracquart@users.noreply.github.com>
2023-11-30 08:59:13 +0100

    Update PageErreur.php

--- src/php/logement/PageErreur.php
+++ src/php/logement/PageErreur.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Feuilles de style -->
-    <link rel="stylesheet" type="text/css" href="/src/styles/titre.css">
+    <link rel="stylesheet" type="text/css" href="/src/styles/pageErreurStyle.css">
 
     <!-- Titre de la page -->
     <title>ALHaiz Breizh</title>
