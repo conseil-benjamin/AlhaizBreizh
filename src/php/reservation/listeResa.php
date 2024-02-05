6b694fdf8b9dee251eecd41bf221cdc41c71261f
Nathan Bracquart <122649974+nbracquart@users.noreply.github.com>
2024-02-05 08:59:13 +0100

    Update listeResa.php

--- src/php/reservation/listeResa.php
+++ src/php/reservation/listeResa.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['id'])) {
             proprio.lastName as nom_proprio FROM ldc.Reservation NATURAL JOIN ldc.Logement inner JOIN ldc.Client on ldc.Reservation.numClient=idCompte INNER JOIN
             ldc.Client as proprio ON proprio.idCompte = Logement.proprio WHERE Client.idCompte = $id;");
 
-    //requete github 2h
+//requete github 2h
     //$stmt = $pdo->prepare("SELECT DISTINCT numLogement,libelle,dateDebut,dateFin,idCompte, pseudoCompte,proprio,numReservation FROM ldc.Reservation NATURAL JOIN ldc.Logement NATURAL JOIN ldc.Client WHERE proprio = $id ");
 
         $stmt->execute();
@@ -38,10 +38,61 @@ if (isset($_SESSION['id'])) {
 
 } else {
 
-    // Redirige l'utilisateur vers la page de connexion
-    header("Location: /src/php/connexion/connexion.php");
-    exit();
+    // Affichez un message d'erreur
+    ?><!DOCTYPE html>
+    <html lang="fr-fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/src/styles/styles.css">
+        <link rel="stylesheet" type="text/css" href="/src/styles/mesLogements.css">
+        
+        <link rel="icon" href="/public/logos/logo-black.svg">
+        <title>ALHaiz Breizh</title>
+</head>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/php/header.php'); ?>
+                    <div class="wrapper">
+                        <video autoplay playsinline muted loop preload poster="http://i.imgur.com/xHO6DbC.png">
+                            <source src="/public/videos/video-bretagne.mp4" />
+                        </video>
+                        <div class="container">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 285 80" preserveAspectRatio="xMidYMid slice">
+                                <defs>
+                                    <mask id="mask" x="0" y="0" width="100%" height="100%">
+                                        <rect x="0" y="0" width="100%" height="100%" />
+                                        <!-- Texte principal -->
+                                        <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-family="NoirPro" font-weight="200" text-transform="uppercase" font-size="20">
+                                            ALHaIZ Breizh
+                                        </text>
+                                    </mask>
+                                </defs>
+                                <!-- Rectangle pour masquer le texte principal -->
+                                <rect x="0" y="0" width="100%" height="100%" mask="url(#mask)" />
+                            </svg>
+                            <!-- Lien vers la page d&#39;accueil avec un message d&#39;erreur -->
+                            <a class="lien" href="/accueil" target="_blank">
+                                <div>
+                                    Cette page est inexistante. Cliquez ici pour retourner à l&#39;accueil.
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
+        <style>
+     .recherche {
+         display: none;
+        }
+        .options {
+                display: none;
+            }
+    body{
+        min-height:0;
+    }
+        </style>
+    </body>
+<?php
 }
 
 $reservations = obtenirLogementsProprio($_SESSION['id']);
@@ -50,82 +101,57 @@ $reservations = obtenirLogementsProprio($_SESSION['id']);
 
 <!DOCTYPE html>
 <html lang="fr-fr">
-
 <head>
-    <!-- Balises meta pour le jeu de caractères, la compatibilité et la vue -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Feuilles de style -->
     <link rel="stylesheet" type="text/css" href="/src/styles/styles.css">
-    <link rel="stylesheet" type="text/css" href="/src/styles/index.css">
-    <link rel="stylesheet" type="text/css" href="/src/styles/style_Liste_resa.css">
-
-    <!-- Titre de la page -->
+    <link rel="stylesheet" type="text/css" href="/src/styles/mesLogements.css">
+    
+    <link rel="icon" href="/public/logos/logo-black.svg">
     <title>ALHaiz Breizh</title>
 </head>
-
-<!-- Section En-tête -->
-<?php include($_SERVER['DOCUMENT_ROOT'].'/src/php/header.php'); ?>
-
-<!-- Section Corps -->
 <body>
-    <!-- Section Contenu -->
-    <div class="content">
-        <!-- Recherche et options de réservation -->
-        <div class="recherche">
-            <h1>Mes Réservations</h1>
-            <div id="options">
-                <div>
-                    <input type="text" placeholder="Rechercher..">
-                    <button class="boutton-tri">Date</button>
-                    <button class="boutton-tri">Prix</button>
-                </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/php/header.php'); ?>
+    <div id="content">
+        <h2>Mes réservations</h2>
+        <div id="options">
+            <div>
+                <input class="textfield" type="text" placeholder="Rechercher..">
+                <button class="boutton">Filtrer</button>
+                <button class="boutton">Trier</button>
             </div>
         </div>
-  
-      
-        <!-- Affiche les cartes de réservation -->
-        <?php foreach ($reservations as $reservation): ?>
-            <div class="card-container">    
-                <div class="reservation-card">
-                    <a href="/src/php/logement/PageDetailLogement.php?numLogement=<?php echo $reservation[0] ?>">
-                        <div class="logement">
+        <div id="logements">
+            <?php 
+            if (count($reservations) === 0) { ?>
+                <h3>Vous n'avez aucune réservation pour le moment :/</h3> <?php
+            } else {
+                foreach ($reservations as $reservation){ ?>
+                    <div class="logement">
                             <img src="/public/img/logements/<?php echo $reservation[0]; ?>/1.png" alt="Photo du logement">
+                        <div>
+                            <h2><?php echo $reservation[1]; ?></h2>
+                            <a href="/src/php/afficherPlageDispo.php?dateDebut=<?php echo $reservation[2] ?>&dateFin=<?php echo $reservation[3] ?>">
+                                <h4><?php echo $reservation[2]; ?></h4>
+                                <h4><?php echo $reservation[3]; ?></h4>
+                            </a>
+                            <a href="/src/php/profil/profil.php?user=<?php echo $reservation[6] ?>">
+                                <div class="profile">
+                                    <p><?php echo "$reservation[8]"." $reservation[9]"; ?></p>
+                                </div>
+                            </a>
+                            <nav>
+                                <a class="boutton" href="/src/php/reservation/details_reservation.php?numReservation=<?php echo $reservation[7]?>">Voir Réservation</a>
+                            </nav>
                         </div>
-                    </a>
-                    <div class="infos">
-                        <h2><?php echo $reservation[1]; ?></h2>
-                        <div class="details">
-                            <p>Date d'arrivée : <?php echo $reservation[2]; ?></p>
-                            <p>Date de départ : <?php echo $reservation[3]; ?></p>
-                        </div>
-                        <a href="/src/php/profil/profil.php?user=<?php echo $reservation[6] ?>">
-                            <div class="profile">
-                                <img src="/public/img/photos_profil/<?php echo $reservation[6]; ?>.png" alt="Photo de profil">
-                                <p><?php echo "$reservation[8]"." $reservation[9]"; ?></p>
-                            </div>
-                        </a>
                     </div>
-                    <label class="button-etat" for="status">État Réservation</label>
-                    <div>
-                        <button class="rond-button">
-                            <div class="contract">
-                                <img src="/public/icons/contract.svg" alt="">
-                            </div>
-                        </button>
-                    </div>
-                    <div>
-                    <a  href="/src/php/reservation/details_reservation.php?numReservation=<?php echo $reservation[7]?>" class="button-resa">Voir Réservation</a>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
+                <?php
+                }
+            }
+            ?>
+        </div>
     </div>
-
-    <!-- Section Pied de page -->
     <?php include($_SERVER['DOCUMENT_ROOT'].'/src/php/footer.php'); ?>
 </body>
-
 </html>
