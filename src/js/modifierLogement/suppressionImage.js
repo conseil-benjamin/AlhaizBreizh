2c7a325c5325a0deb8aad509f76c6c3270dfbd8b
FlorianNormand <flo1.normand@gmail.com>
2024-04-03 08:54:52 +0200

    fix bug suppresion image creation logement

--- src/js/modifierLogement/suppressionImage.js
+++ src/js/modifierLogement/suppressionImage.js
@@ -57,15 +57,55 @@ function supprimerPhoto(numero) {
     });
 }
 
+//Cette fonction permet de supprimer une photo de la liste des photos à uploader
+function supprimerPhotoInput(img) {
+    // Confirme la suppression
+    Swal.fire({
+      icon: 'warning',
+      title: 'Confirmation de suppression',
+      text: 'Êtes-vous sûr de vouloir supprimer cette photo ?',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmer',
+      cancelButtonText: 'Annuler',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        var numero = img.getAttribute('data-numero');
+  
+        // Suppression de l'image du DOM
+        const photoContainer = img.parentNode.parentNode;
+        photoContainer.parentNode.removeChild(photoContainer);
+  
+        const input = document.getElementById('photos');
+        const files = input.files;
+  
+        const newFiles = [];
+        for (let i = 0; i < files.length; i++) {
+          if (i !== (numero - 1)) {
+            newFiles.push(files[i]);
+          }
+        }
+  
+        input.files = newFiles;
+  
+        Swal.fire({
+          icon: 'success',
+          title: 'Photo supprimée avec succès.',
+          showConfirmButton: false,
+          timer: 2000
+        });
+      }
+    });
+  }
+  
+
 function previewImage() {
     const input = document.getElementById('photos');
     const files = input.files;
-  
     const photosContainer = document.querySelector('.listePhotos');
+    let numeroPhoto = parseInt(localStorage.getItem('numeroPhoto')) || 1;
   
     for (const file of files) {
       const reader = new FileReader();
-  
       reader.onload = function(e) {
         const photoContainer = document.createElement('div');
         photoContainer.classList.add('photoContainer');
@@ -83,18 +123,17 @@ function previewImage() {
         croix.src = "/public/icons/croix.svg"; 
         croix.classList.add('croix');
         croix.style.display = 'none'; 
-        croix.setAttribute('onclick', `supprimerPhoto(this)`); // Définir le gestionnaire onclick avec le nom du fichier en argument
-  
+        croix.setAttribute('onclick', `supprimerPhotoInput(this)`);
+        croix.setAttribute('data-numero', numeroPhoto);
+        numeroPhoto++;
   
         photo.appendChild(img);
         photo.appendChild(croix);
-
         photoContainer.appendChild(photo);
-  
         photosContainer.appendChild(photoContainer);
       };
-  
       reader.readAsDataURL(file);
     }
+    localStorage.setItem('numeroPhoto', numeroPhoto);
   }
   
